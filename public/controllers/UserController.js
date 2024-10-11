ed942fad1aaeb83dcf293d0ded1f66571a929659
Aurelien <aurelien.fillion.pro@gmail.com>
2024-10-11 14:50:46 +0200

    fix: passage de docker local

--- public/controllers/UserController.js
+++ public/controllers/UserController.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const User = require("../models/User");
 const Hash = require("../utils/Hash");
+const jwt = require("jsonwebtoken");
 
 class UserController {
   static async getUsers(req, res) {
@@ -20,18 +21,44 @@ class UserController {
     }
   }
 
-static async createUser(req, res) {
-  try {
-    let { email, name, password } = req.body;
-    password = await Hash.hash(password);
-    const user = new User({ email, name, password });
-    await user.save();
-    res.status(201).send(user); // Renvoie l'utilisateur créé avec un statut 201
-    console.log("User created successfully:");
-  } catch (e) {
-    res.status(400).send({ error: e.stack }); // Envoie le message d'erreur en cas d'échec
+  static async createUser(req, res) {
+    try {
+      let { email, name, password } = req.body;
+      password = await Hash.hash(password);
+      const user = new User({ email, name, password });
+      await user.save();
+      res.status(201).send(user); // Renvoie l'utilisateur créé avec un statut 201
+      console.log("User created successfully:");
+    } catch (e) {
+      res.status(400).send({ error: e.stack }); // Envoie le message d'erreur en cas d'échec
+    }
   }
-}
+
+  static async loginUser(req, res) {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if (!user) {
+        res.status(401).send({ error: "Invalid email or password" });
+        return;
+      }
+
+      const isPasswordValid = await Hash.compare(password, user.password);
+      if (!isPasswordValid) {
+        res.status(401).send({ error: "Invalid email or password" });
+        return;
+      }
+
+      // Générer un token JWT
+      const token = jwt.sign({ userId: user._id }, "jwt_secret", {
+        expiresIn: "1h",
+      });
+
+      res.send({ message: "Login successful", token });
+    } catch (e) {
+      res.status(500).send({ error: "Internal Server Error" });
+    }
+  }
 
   static async getCurrentUser(req, res) {
     console.log("req.user", req.user);
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,13 @@
-var express = require('express');
+var express = require("express");
 const router = express.Router();
-const User = require('../public/models/User');
-const UserController = require('../public/controllers/UserController');
+const User = require("../public/models/User");
+const UserController = require("../public/controllers/UserController");
 
-
-//ROUTE USER
+// ROUTE USER
 router.get("/", UserController.getUsers);
 router.get("/@me", UserController.getCurrentUser);
 router.get("/:id", UserController.getUserById);
 router.post("/signup", UserController.createUser);
-
-
+router.post("/login", UserController.loginUser);
 
 module.exports = router;
